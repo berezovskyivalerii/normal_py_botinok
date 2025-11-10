1f0f69ce436580300e473400d3cffb3913559182
valerii <berezovskijvalera812@example.com>
2025-11-10 09:00:09 +0100

    debug tests

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,7 +4,7 @@ def test_smoke():
     assert True
 
 def test_calculate_addition():
-    assert calculate_expression('1 + 1 + 1') == 3
+    assert calculate_expression('1 + 1 + 1') == '3'
 
 def test_calculate_sunstraction():
-    assert calculate_expression('2-3') == -1
+    assert calculate_expression('2-3') == '-1'
